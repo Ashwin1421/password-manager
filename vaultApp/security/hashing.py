5cc35813113fd4d3eb86022a5aedaee4532f4296
Ashwin Joshi <ashwin.joshi12@live.com>
2018-10-04 13:55:58 -0400

    feature: added bcrypt secure hashing for storing plaintext passwords

--- vaultApp/security/hashing.py
+++ vaultApp/security/hashing.py
@@ -1,17 +1,24 @@
 from hashlib import sha1, sha256, sha384, sha512
+from bcrypt import hashpw, gensalt
+from base64 import b64encode
+from os import urandom
+
 class PasswordHashing():
 
     def __init__(self, algorithm="sha256"):
         self.__algorithm = algorithm
-        if algorithm.__eq__("sha1"):
-            self.__sha  = sha1()
-        elif algorithm.__eq__("sha384"):
-            self.__sha = sha384()
-        elif algorithm.__eq__("sha512"):
-            self.__sha = sha512()
+        if algorithm == "sha1":
+            self.__sha = sha1
+        elif algorithm == "sha384":
+            self.__sha = sha384
+        elif algorithm == "sha512":
+            self.__sha = sha512
         else:
-            self.__sha = sha256()
+            self.__sha = sha256
 
     def get_hash_value(self, input):
-        self.__sha.update(input.encode())
-        return self.__sha.hexdigest()
+        hashed = hashpw(
+            b64encode(self.__sha(input.encode()+urandom(len(input))).digest()),
+            gensalt(16)
+               )
+        return hashed.decode()

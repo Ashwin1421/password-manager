848d4fe0ff0aca63d9ea40af8c6fcfbb3c1d4282
Ashwin Joshi <ashwin.joshi12@live.com>
2018-10-05 16:09:08 -0400

    patch: code refactoring, minor improvements

--- vaultApp/forms.py
+++ vaultApp/forms.py
@@ -23,10 +23,24 @@ class SignupForm(UserCreationForm):
         fields = ('email', 'first_name', 'last_name', 'password1', 'password2',)
 
 
-class PasswordEntryCreationForm(forms.Form):
-    username = forms.CharField(max_length=150, required=True, help_text="Required")
-    plaintext_password = forms.CharField(max_length=128, required=True, widget=forms.PasswordInput, help_text="Required")
+class PasswordEntryForm(forms.Form):
+    website = forms.CharField(max_length=150, required=True)
+    username = forms.CharField(max_length=150, required=True)
+    plaintext_password = forms.CharField(max_length=128, required=True, widget=forms.PasswordInput)
 
     class Meta:
         model = PasswordEntry
-        fields = ('username', 'plaintext_password', 'date_of_creation')
+        fields = ('username', 'plaintext_password', 'website', 'date_of_creation')
+
+
+class RandomPasswordGenerationForm(forms.Form):
+    random_plaintext_password = forms.CharField(
+        max_length=150,
+        required=True,
+        help_text="Required",
+        label="Generate new random password"
+    )
+
+    class Meta:
+        model = PasswordEntry
+        fields = ('random_plaintext_password',)
